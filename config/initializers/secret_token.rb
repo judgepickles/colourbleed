3998a6034481c36b48d93fe5bd84e57a41504e2f
Mark Sadegi <mark.sadegi@gmail.com>
2011-02-08 08:04:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colourbleed::Application.config.secret_token = '6838897836dc91b3f716f45bfe9da76b68118a4d6101a71034ff7f1503ce5e7b0df780b572878e644776f9879ce01e69fe0b97e03a2acaf8672cb986d4a8e366'
