94c60e5018d15ca52665187a03fa43ca14eb2bdb
Mark Sadegi <mark.sadegi@gmail.com>
2011-02-08 12:18:09 -0600

    added title

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,6 @@
 Colourbleed::Application.routes.draw do
 
 
-  get "pages/home"
-
-  get "pages/trailer"
-
-  get "pages/story"
-
-  get "pages/gallery"
-
-  get "pages/cast_crew"
-
-  get "pages/behind_the_scenes"
-
-  get "pages/press"
-
-  get "pages/contact"
-
   root :to => "pages#home"
 
   match '/trailer',             :to => 'pages#trailer'
--- spec/views/pages/behind_the_scenes.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "pages/behind_the_scenes.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
