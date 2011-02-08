985912d2ad46324389ba08dd0706c0581f82f791
Mark Sadegi <mark.sadegi@gmail.com>
2011-02-08 17:08:52 -0600

    did a quick rippoff to get an idea of the positional elements

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Colourbleed::Application.routes.draw do
   match '/trailer',             :to => 'pages#trailer'
   match '/story',               :to => 'pages#story'
   match '/gallery',             :to => 'pages#gallery'
-  match '/cast-crew',           :to => 'pages#cast_crew'
-  match '/behind-the-scenes',   :to => 'pages#behind_the_scenes'
+  match '/cast_crew',           :to => 'pages#cast_crew'
+  match '/behind_the_scenes',   :to => 'pages#behind_the_scenes'
   match '/press',               :to => 'pages#press'
   match '/contact',             :to => 'pages#contact'
 
