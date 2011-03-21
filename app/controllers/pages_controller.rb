6699ef4aad8d46f033426b7399d6ad9ddea48232
Mark Sadegi <mark.sadegi@gmail.com>
2011-03-21 12:52:46 -0500

    added & amended news

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
   layout proc { |controller| controller.request.xhr? ? false : "application" }
 
+  def index
+    response.headers['Cache-Control'] = 'public, max-age=300'
+        render :text => "Rendered at #{Time.now}"
+  end
+
   def home
     @title = "Home"
     respond_to(:html)
+    # response.headers['Cache-Control'] = 'public, max-age=300'
+    # render :text => "Rendered at #{Time.now}"
   end
 
   def trailer
