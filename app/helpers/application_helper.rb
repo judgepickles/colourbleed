945d6622d73351542d672bca6dfec4c94f90abd4
Mark Sadegi <mark.sadegi@gmail.com>
2011-04-06 17:29:11 -0500

    facebook_like

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def facebook_like
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=80&action=like&font=arial&colorscheme=light&height=25",
+                :scrolling => 'no', 
+                :frameborder => '0', 
+                :allowtransparency => true, 
+                :id => :facebook_like
+  end
 end
