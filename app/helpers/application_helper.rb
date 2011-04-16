e86b777dee6e4cf80982e682aea03ff486136de4
Mark Sadegi <mark.sadegi@gmail.com>
2011-04-16 11:50:57 -0500

    fb like deletion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def facebook_like
-    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FColourBleed%2F177383978978025&amp;width=292&amp;colorscheme=dark&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62",
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FColourBleed%2F177383978978025&amp;width=352&amp;colorscheme=dark&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=122",
                 :scrolling => 'no',
                 :overflow => 'hidden', 
                 :frameborder => '0',
